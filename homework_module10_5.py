ce4216455b847b3a57331c59398bfdc6f8af1f32
Ilgiz <72934972+alfa1991@users.noreply.github.com>
2024-09-07 22:36:21 +0500

    Add files via upload

---
+++ homework_module10_5.py
@@ -0,0 +1,28 @@
+import time
+from multiprocessing import Pool
+
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            all_data.append(line)
+
+
+if __name__ == '__main__':
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+    # Линейный вызов
+    start_time = time.time()
+    for filename in filenames:
+        read_info(filename)
+    print(f"Линейный вызов: {time.time() - start_time}")
+
+    # Многопроцессный вызов
+    # start_time = time.time()
+    # with Pool() as pool:
+    #     pool.map(read_info, filenames)
+    # print(f"Многопроцессный вызов: {time.time() - start_time}")
